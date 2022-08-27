4bfab1a7164d541592ad9b7083adbc287e9227d2
[mallikarjuna annigeri] <mallikarjunaannigeri27@gmail.com>
2022-08-27 14:28:14 +0530

    added

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+// const port = process.env.PORT || 8000;
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
