d7cc57d90b8b0c365c1fabd36e3f47bd0e2dab8d
[mallikarjuna annigeri] <mallikarjunaannigeri27@gmail.com>
2022-09-17 13:06:07 +0530

    json -server running...

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-// const port = process.env.PORT || 8000;
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 8000;
+  port = 8080;
 }
 
 server.use(middlewares);
 server.use(router);
-
 server.listen(port);
